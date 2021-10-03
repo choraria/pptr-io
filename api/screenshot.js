3b98b7237bf3a8ad66f4a5025a4e8b2c106b81d7
Sourabh Choraria <sourabh.choraria@dashdash.com>
2021-10-03 14:53:04 +0530

    capture error

--- api/screenshot.js
+++ api/screenshot.js
@@ -1,16 +1,24 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 module.exports = async (req, res) => {
-  const url = req.query.url;
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-  const file = await page.screenshot({
-    type: "png",
-  });
-  await browser.close();
+  try {
+    const url = req.query.url;
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const file = await page.screenshot({
+      type: "png",
+    });
+    await browser.close();
 
-  res.statusCode = 200;
-  res.setHeader("Content-Type", `image/png`);
-  res.end(file);
+    res.statusCode = 200;
+    res.setHeader("Content-Type", `image/png`);
+    res.end(file);
+  } catch (err) {
+    console.log(err);
+    res.statusCode = 500;
+    res.json({
+      error: err.toString(),
+    });
+  }
 };
