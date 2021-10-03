ca135995a497fda53010635b4c44f3e988258937
Sourabh Choraria <sourabh.choraria@dashdash.com>
2021-10-03 15:57:04 +0530

    adding metrics

---
+++ api/metrics.js
@@ -0,0 +1,42 @@
+const puppeteer = require("puppeteer-core");
+const chrome = require("chrome-aws-lambda");
+
+module.exports = async (req, res) => {
+  try {
+
+    let urlToScreenshot;;
+    try {
+      urlToScreenshot = new URL({ toString: () => req.query.url });
+    } catch (e) {
+      res.statusCode = 400;
+      res.json({
+        error: "Invalid URL"
+      });
+    }
+
+    const browser = await puppeteer.launch({
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      ignoreHTTPSErrors: true,
+    });
+    const page = await browser.newPage();
+
+    await page.goto(urlToScreenshot, {
+      waitUntil: 'networkidle2',
+    });
+    const metrics = await page.metrics();
+    await browser.close();
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", `application/json`);
+    res.end(JSON.stringify(metrics));
+    
+  } catch (err) {
+    console.log(err);
+    res.statusCode = 500;
+    res.json({
+      error: err.toString(),
+    });
+  }
+};
