e99291df30721122e6745965d1fb86284724efec
Sourabh Choraria <sourabh.choraria@dashdash.com>
2021-10-06 22:30:33 +0530

    testing changes to dev/screenshot

---
+++ api/dev/screenshot.js
@@ -0,0 +1,72 @@
+const puppeteer = require("puppeteer-core");
+const chrome = require("chrome-aws-lambda");
+
+const DEFAULT_VIEWPORT = { height: 1080, width: 1920, deviceScaleFactor: 1 };
+const ALLOWED_FILE_TYPES = ["jpeg", "webp", "png"];
+const DEFAULT_FILE_TYPE = "png";
+
+const merge = (a, b) =>
+  Object.assign(
+    {},
+    a,
+    ...Object.entries(b).map(([k, v]) =>
+      v === undefined || (typeof v === "number" && isNaN(v)) || v === null ? {} : { [k]: v }
+    )
+  );
+const toBoolean = (val) => val === "true";
+
+module.exports = async (req, res) => {
+  try {
+    let url;
+    try {
+      url = new URL({ toString: () => req.query.url });
+    } catch (e) {
+      res.statusCode = 400;
+      res.json({
+        error: "Invalid URL",
+      });
+    }
+    const fullPage = toBoolean(req.query.fullPage);
+    const screenshotFileType = req.query.type;
+    if (screenshotFileType && !ALLOWED_FILE_TYPES.includes(screenshotFileType)) {
+      res.statusCode = 400;
+      res.json({
+        error: "invalid file type requested",
+      });
+    }
+    const userViewPortInputParams = {
+      height: Number(req.query.height),
+      width: Number(req.query.width),
+      deviceScaleFactor: Number(req.query.deviceScaleFactor),
+    };
+    const viewPortParams = merge(DEFAULT_VIEWPORT, userViewPortInputParams);
+    const browser = await puppeteer.launch({
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      ignoreHTTPSErrors: true,
+    });
+    const page = await browser.newPage();
+
+    await page.setViewport(viewPortParams);
+
+    await page.goto(url, {
+      waitUntil: "networkidle2",
+    });
+    const file = await page.screenshot({
+      type: screenshotFileType || DEFAULT_FILE_TYPE,
+      fullPage: fullPage,
+    });
+    await browser.close();
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", `image/${screenshotFileType || DEFAULT_FILE_TYPE}`);
+    res.end(file);
+  } catch (err) {
+    console.log(err);
+    res.statusCode = 500;
+    res.json({
+      error: err.toString(),
+    });
+  }
+};
