de800ca2fa215c4f90d8aeaf7ce9f5e4a402ecc3
Sourabh Choraria <sourabh.choraria@dashdash.com>
2021-10-06 23:35:56 +0530

    removed lowercase

--- api/screenshot.js
+++ api/screenshot.js
@@ -15,7 +15,7 @@ module.exports = async (req, res) => {
       });
     }
     const fullPage =  req.query.fullPage.toString().toLowerCase() == "true" ? true : false;
-    const screenshotFileType = req.query.type.toLowerCase();
+    const screenshotFileType = req.query.type;
     const fileType = ALLOWED_FILE_TYPES.includes(screenshotFileType) ? screenshotFileType : "png";
 
     const browser = await puppeteer.launch({
