c482246dec29cd6a76342e5e1c6ce10e32984b87
Sourabh Choraria <sourabh.choraria@dashdash.com>
2021-10-09 12:16:10 +0530

    adding pdf.js

---
+++ api/pdf.js
@@ -0,0 +1,40 @@
+const puppeteer = require("puppeteer-core");
+const chrome = require("chrome-aws-lambda");
+
+module.exports = async (req, res) => {
+  try {
+    let url;
+    try {
+      url = new URL({ toString: () => req.query.url });
+    } catch (e) {
+      res.statusCode = 400;
+      res.json({
+        error: "Invalid URL",
+      });
+    }
+
+    const browser = await puppeteer.launch({
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      ignoreHTTPSErrors: true,
+    });
+    const page = await browser.newPage();
+
+    await page.goto(url, {
+      waitUntil: "networkidle2",
+    });
+    const pdf = await page.pdf();
+    await browser.close();
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", `application/pdf`);
+    res.end(pdf);
+  } catch (err) {
+    console.log(err);
+    res.statusCode = 500;
+    res.json({
+      error: err.toString(),
+    });
+  }
+};
