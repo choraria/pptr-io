b076c8fd4b4b7a5aab9cc718317a40d67dad133b
Sourabh Choraria <sourabh.choraria@dashdash.com>
2021-10-09 20:35:51 +0530

    adding itemprop as key and updating readme

--- api/meta.js
+++ api/meta.js
@@ -29,7 +29,7 @@ module.exports = async (req, res) => {
         const data = {};
         const metaTags = document.querySelectorAll('meta');
         metaTags.forEach((tag, i) => {
-            const key = tag.getAttribute('name') ? tag.getAttribute('name') : (tag.getAttribute('property') ? tag.getAttribute('property') : tag.getAttribute('http-equiv'));
+            const key = tag.getAttribute('name') ? tag.getAttribute('name') : (tag.getAttribute('property') ? tag.getAttribute('property') : (tag.getAttribute('http-equiv') ? tag.getAttribute('http-equiv') : tag.getAttribute('itemprop')));
             tag.getAttribute('charset') ? data["charset"] = tag.getAttribute('charset') : data[key == null ? i : key] = tag.getAttribute('content');
         });
         return data;
