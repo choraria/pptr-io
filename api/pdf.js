68130fecadb674a19089d40c893b7a32abb400d0
Sourabh Choraria <code@script.gs>
2021-10-11 23:05:35 +0530

    Update pdf.js

--- api/pdf.js
+++ api/pdf.js
@@ -8,24 +8,25 @@ module.exports = async (req, res) => {
   try {
     const url = req.query.url;
 
-    const scale = req.query.scale ? Number(req.query.scale) <= 2 && Number(req.query.scale) >= 0.1 ? Number(req.query.scale) : 1 : 1;
-    const printBackground = req.query.printBackground ? req.query.printBackground.toString().toLowerCase() === "true" ? true : false : false;
-    const landscape = req.query.landscape ? req.query.landscape.toString().toLowerCase() === "true" ? true : false : false;
-    const format = ALLOWED_FORMATS.includes(req.query.format?.toString()) ? req.query.format.toString() : undefined; // case sensitive
-    
-    const widthRaw = req.query.width?.split(/([0-9.]+)/)?.filter(s => s !== ""); // splits into numerical value and unit ("10cm" -> ["10","cm"])
+    const scale = req.query.scale ? (Number(req.query.scale) <= 2 && Number(req.query.scale) >= 0.1 ? Number(req.query.scale) : 1) : 1;
+    const landscape = req.query.landscape ? (req.query.landscape.toString().toLowerCase() === "true" ? true : false) : false;
+    const format = ALLOWED_FORMATS.includes(req.query.format?.toString()) ? req.query.format.toString() : undefined;
+
+    const widthRaw = req.query.width ?.split(/([0-9.]+)/) ?.filter((s) => s !== "");
     let width;
-    if(widthRaw?.length === 1 && Number(widthRaw[0])) 
-    width = Number(widthRaw[0]);
-    else if(widthRaw?.length === 2 && Number(widthRaw[0]) && ALLOWED_UNITS.includes(widthRaw[1].toLowerCase())) 
-    width = widthRaw.join("").toLowerCase();
-    
-    const heightRaw = req.query.width?.split(/([0-9.]+)/)?.filter(s => s !== ""); // splits into numerical value and unit ("10cm" -> ["10","cm"])
+    if (widthRaw?.length === 1 && Number(widthRaw[0])) {
+      width = Number(widthRaw[0]);
+    } else if (widthRaw?.length === 2 && Number(widthRaw[0]) && ALLOWED_UNITS.includes(widthRaw[1].toLowerCase())) {
+      width = widthRaw.join("").toLowerCase();
+    }
+
+    const heightRaw = req.query.width ?.split(/([0-9.]+)/) ?.filter((s) => s !== "");
     let height;
-    if(heightRaw?.length === 1 && Number(heightRaw?.[0])) 
-    height = Number(heightRaw[0]);
-    else if(heightRaw?.length === 2 && Number(heightRaw[0]) && ALLOWED_UNITS.includes(heightRaw[1].toLowerCase())) 
-    height = heightRaw.join("").toLowerCase();
+    if (heightRaw?.length === 1 && Number(heightRaw?.[0])) {
+      height = Number(heightRaw[0]);
+    } else if (heightRaw?.length === 2 && Number(heightRaw[0]) && ALLOWED_UNITS.includes(heightRaw[1].toLowerCase())) {
+      height = heightRaw.join("").toLowerCase();
+    }
 
     const browser = await puppeteer.launch({
       args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
@@ -41,12 +42,12 @@ module.exports = async (req, res) => {
 
     const pdf = await page.pdf({
       pageRanges: "1",
-      printBackground: printBackground,
+      printBackground: true,
       landscape: landscape,
-      format: (!format && !width && !height ? "Letter" : undefined),
+      format: !format && !width && !height ? "Letter" : format,
       height: height,
-      width: width, 
-      scale: scale
+      width: width,
+      scale: scale,
     });
     await browser.close();
 
