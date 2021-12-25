64efed79da3d8dc9c15aa731cd2c37844fb0f3b5
Sourabh Choraria <sourabh.choraria@dashdash.com>
2021-12-26 00:34:07 +0530

    minor edit

--- api/instagram.js
+++ api/instagram.js
@@ -20,7 +20,7 @@ module.exports = async (req, res) => {
 
     res.statusCode = 200;
     res.setHeader("Content-Type", `application/json`);
-    res.end(JSON.stringify(data));
+    res.end(data);
   } catch (err) {
     console.log(err);
     res.statusCode = 500;
