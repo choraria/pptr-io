3683e537d9e59f68ca1a8e35960e56c1b77657b7
Sourabh Choraria <sourabh.choraria@dashdash.com>
2021-12-26 00:31:19 +0530

    access instagram json data

---
+++ api/instagram.js
@@ -0,0 +1,32 @@
+const puppeteer = require("puppeteer-core");
+const chrome = require("chrome-aws-lambda");
+
+module.exports = async (req, res) => {
+  try {
+    const username = req.query.username;
+
+    const browser = await puppeteer.launch({
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      ignoreHTTPSErrors: true,
+    });
+    const page = await browser.newPage();
+    const url = `https://www.instagram.com/${username}/?__a=1`;
+
+    const data = await page.goto(url);
+
+    await browser.close();
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", `application/json`);
+    res.end(JSON.stringify(data));
+  } catch (err) {
+    console.log(err);
+    res.statusCode = 500;
+    res.json({
+      error: err.toString(),
+    });
+    res.end();
+  }
+};
